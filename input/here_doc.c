5a9302df2689845bb96089298435fc525b44e70e
Chaisneau <chaisneau@gmail.com>
2025-01-09 04:47:23 +0100

    heredoc with no name fixed

--- input/here_doc.c
+++ input/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: achaisne <achaisne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 22:27:35 by achaisne          #+#    #+#             */
-/*   Updated: 2025/01/05 01:40:03 by achaisne         ###   ########.fr       */
+/*   Updated: 2025/01/09 04:44:45 by achaisne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	build_here_doc(t_str *str, char *delimiter)
 	char	*line;
 
 	line = readline(">");
-	while (line && ft_strncmp(line, delimiter, ft_strlen(delimiter)) != 0)
+	while (line && ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1) != 0)
 	{
 		if (!ft_str_push(str, line, ft_strlen(line)))
 			return (0);
@@ -44,7 +44,7 @@ int	set_here_doc(char *delimiter)
 	buff = ft_str_get_char_array(str, str->size - str->start);
 	if (!buff)
 		return (0);
-	tmp_file = open(delimiter, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	tmp_file = open("here_doc", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (!tmp_file)
 		return (free(buff), ft_str_free(str), 0);
 	write(tmp_file, buff, str->size - str->start);
@@ -57,7 +57,7 @@ int	get_here_doc(char *delimiter)
 
 	if (!set_here_doc(delimiter))
 		return (0);
-	fd_in = open(delimiter, O_RDONLY);
+	fd_in = open("here_doc", O_RDONLY);
 	if (fd_in == -1)
 	{
 		perror(delimiter);
@@ -65,7 +65,7 @@ int	get_here_doc(char *delimiter)
 		if (fd_in == -1)
 			return (perror("Failed to open /dev/null"), 0);
 	}
-	if (unlink(delimiter) == -1)
+	if (unlink("here_doc") == -1)
 		return (close(fd_in), 0);
 	return (fd_in);
 }
